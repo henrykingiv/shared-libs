ec1e1e7b8cc791fde31a06be940c398c2d07c484
henrykingiv <97360518+henrykingiv@users.noreply.github.com>
2024-06-26 16:54:08 +0100

    Update checkoutCode.groovy

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,6 +1,9 @@
 def call(String repoUrl, String branch){
-// def workingDir = "${env.WORKSPACE}"
+def workingDir = "${env.WORKSPACE}"
+sh "rm -rf ${workingDir}"
 sh "git clone ${repoUrl} ${workingDir}"
+dir(workingDir) {
 sh "git checkout ${branch}"
+}
 return workingDir
 }
