a062cbcd2923f7bfdf0c4482590fe9447b9f1b52
henrykingiv <97360518+henrykingiv@users.noreply.github.com>
2024-06-26 20:58:18 +0100

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,3 +1,19 @@
 def call(String imageName, String registry = '', String credentialsId = '') {
     DockerUtils.push(imageName, registry, credentialsId)
 }
+
+class DockerUtils {
+    static void push(String imageName, String registry, String credentialsId) {
+        if (registry) {
+            imageName = "${registry}/${imageName}"
+        }
+
+        if (credentialsId) {
+            withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                sh "echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin ${registry}"
+            }
+        }
+
+        sh "docker push ${imageName}"
+    }
+}
