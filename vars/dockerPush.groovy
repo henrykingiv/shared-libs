c0db922e87e0c147c5553a09fcef6a68ae533469
henrykingiv <97360518+henrykingiv@users.noreply.github.com>
2024-06-26 21:16:06 +0100

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,19 +1,29 @@
 def call(String imageName, String registry = '', String credentialsId = '') {
-    DockerUtils.push(imageName, registry, credentialsId)
+    if (credentialsId) {
+        withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+            DockerUtils.push(imageName, registry, env.DOCKER_USERNAME, env.DOCKER_PASSWORD)
+        }
+    } else {
+        DockerUtils.push(imageName, registry)
+    }
 }
 
 class DockerUtils {
-    static void push(String imageName, String registry, String credentialsId) {
+    static void push(String imageName, String registry = '', String username = '', String password = '') {
         if (registry) {
             imageName = "${registry}/${imageName}"
         }
 
-        if (credentialsId) {
-            withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-                sh "echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin ${registry}"
-            }
+        if (username && password) {
+            sh "echo ${password} | docker login -u ${username} --password-stdin ${registry}"
         }
 
         sh "docker push ${imageName}"
+    }
+
+    private static void sh(String script) {
+        // This is a placeholder for the actual Jenkins sh step
+        // In a Jenkins context, this would be executed as a shell script
+        println "Executing shell script: ${script}"
     }
 }
