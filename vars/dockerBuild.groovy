90526012ad75db677356e1ad25817627327e9994
henrykingiv <97360518+henrykingiv@users.noreply.github.com>
2024-06-26 16:37:04 +0100

    Create dockerBuild.groovy

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,9 @@
+def call(String imageName, String context = ','){
+  dockerBuild(imageName, context)
+}
+
+private void dockerBuild(String imageName, String context) {
+  def cmd = "docker build -t ${imageName} ${context}"
+  echo "Executing: ${cmd}"
+  sh cmd
+}
