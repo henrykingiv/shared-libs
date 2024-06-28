c219c8b73a14c6bff37262589ac4d063db8c1093
henrykingiv <97360518+henrykingiv@users.noreply.github.com>
2024-06-28 19:42:29 +0100

    Update dockerLogin.groovy

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,4 +1,4 @@
-def call(String credentialsId = 'docker-hub-credentials', String registry = 'https://index.docker.io/v1/') {
+def call(String credentialsId = 'docker-creds', String registry = 'https://hub.docker.com/repositories/henrykingiv') {
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
         sh """
             echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin ${registry}
