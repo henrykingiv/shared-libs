52bb00a54dc7a26ee40c0e23b8d5caa063297161
henrykingiv <97360518+henrykingiv@users.noreply.github.com>
2024-06-28 20:52:13 +0100

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -3,28 +3,26 @@ def call(String imageName, String credentialsId = '', String registry = '') {
         echo "Logging into Docker registry: ${registry} with username: ${env.DOCKER_USERNAME}"
         sh """
             echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin ${registry}
-            sh "echo ${password} | docker login -u ${username} --password-stdin ${registry}"
-            sh "docker push ${imageName}"
         """
     }
 }
 
-// class DockerUtils {
-//     static void push(String imageName, String registry = '', String username = '', String password = '') {
-//         if (registry) {
-//             imageName = "${registry}/${imageName}"
-//         }
+class DockerUtils {
+    static void push(String imageName, String registry = '', String username = '', String password = '') {
+        if (registry) {
+            imageName = "${registry}/${imageName}"
+        }
 
-//         if (username && password) {
-//             sh "echo ${password} | docker login -u ${username} --password-stdin ${registry}"
-//         }
+        if (username && password) {
+            sh "echo ${password} | docker login -u ${username} --password-stdin ${registry}"
+        }
 
-//         sh "docker push ${imageName}"
-//     }
+        sh "docker push ${imageName}"
+    }
 
-//     private static void sh(String script) {
-//         // This is a placeholder for the actual Jenkins sh step
-//         // In a Jenkins context, this would be executed as a shell script
-//         println "Executing shell script: ${script}"
-//     }
-// }
+    private static void sh(String script) {
+        // This is a placeholder for the actual Jenkins sh step
+        // In a Jenkins context, this would be executed as a shell script
+        println "Executing shell script: ${script}"
+    }
+}
