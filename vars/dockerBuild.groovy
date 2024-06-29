14d6e677db81e1dbeccbf4666e2860c3449e389e
henrykingiv <97360518+henrykingiv@users.noreply.github.com>
2024-06-29 12:33:14 +0100

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,7 +3,7 @@ def call(String imageName, String context = '.'){
 }
 
 private void dockerBuild(String imageName, String context) {
-  def cmd = "docker build -t ${imageName} ${context}"
+  def cmd = "docker buildx build -t ${imageName} ${context}"
   echo "Executing: ${cmd}"
   sh cmd
 }
