2615048cca0e7e81fe33e8b06efaa599e9f34516
henrykingiv <97360518+henrykingiv@users.noreply.github.com>
2024-07-03 19:06:18 +0100

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,3 +1,7 @@
-def call() {
-  sh "mvn clean package -DskipTests -Dcheckstyle.skip"
+// def call() {
+//   sh "mvn clean package -DskipTests -Dcheckstyle.skip"
+// }
+
+def call(String mavenGoals = 'clean package', String mavenOpts = '-DskipTests -Dcheckstyle.skip') {
+  sh "mvn ${mavenGoals} ${mavenOpts}"
 }
