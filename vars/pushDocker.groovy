3f84fbacb56d1df70d2279fd586af33ecb1712fe
henrykingiv <97360518+henrykingiv@users.noreply.github.com>
2024-07-03 23:27:07 +0100

    Update pushDocker.groovy

--- vars/pushDocker.groovy
+++ vars/pushDocker.groovy
@@ -1,7 +1,28 @@
 def call(String imageName, String credentialsId = 'docker-creds', String toolName = 'docker') {
-    pushDocker(imageName, credentialsId, toolName)
+    DockerPushClass sujit=new DockerPushClass()
+    sujit.pushDocker(imageName, credentialsId, toolName)
+
+    sujit.callingPrivateMethod(imageName, credentialsId, toolName)
+    
+    DockerPushClass.pushDockerStatic(imageName, credentialsId, toolName)
+    
 }
 
-private void pushDocker(String imageName, String credentialsId, String toolName) {
-    sh "docker push ${imageName}"
+class DockerPushClass {
+    public void pushDocker(String imageName, String credentialsId, String toolName) {
+        System.out.println("dockerPushMethod: " + imageName)
+        sh "docker push ${imageName}"
+    }
+
+    static void pushDockerStatic(String imageName, String credentialsId, String toolName) {
+        sh "docker push ${imageName}"
+    }
+
+    private void pushDockerPrivate(String imageName, String credentialsId, String toolName) {
+        sh "docker push ${imageName}"
+    }
+
+    public void callingPrivateMethod(String imageName, String credentialsId, String toolName) {
+        pushDockerPrivate(imageName, credentialsId, toolName)
+    }
 }
